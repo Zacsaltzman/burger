c355514a194910be3558e893ebf3a6d9bdacd6cf
Zachary Saltzman <zsaltzman@Zacharys-MacBook-Pro-2.local>
2017-09-28 16:47:46 -0400

    no port

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,10 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
   connection = mysql.createConnection({
-    port: 3306,
     host: "127.0.0.1",
     user: "root",
     password: "",

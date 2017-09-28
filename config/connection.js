eb2bd1268b4839248ce8af4b3e1958dfeb369ac5
Zachary Saltzman <zsaltzman@Zacharys-MacBook-Pro-2.local>
2017-09-28 16:44:47 -0400

    connection declared alone

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
